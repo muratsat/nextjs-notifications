0fcb72630238f1e8c9db9a00bacf94047a17bcd8
Murat <muratsat@users.noreply.github.com>
2025-03-05 19:17:25 +0600

    remove icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,12 +9,5 @@ export default function Manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
-    icons: [
-      {
-        src: "/icon.png",
-        sizes: "256x256",
-        type: "image/png",
-      },
-    ],
   };
 }
